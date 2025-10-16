edb8727366f8904f597d7161be403a7317a46ccb
Jahidul Islam <jahid.redmi10@gmail.com>
2025-10-16 12:32:50 +0600

    create error page

---
+++ src/app/(comonLayOut)/products/error.tsx
@@ -0,0 +1,21 @@
+"use client";
+const ErrorPage = ({ error, reset }) => {
+  console.log(error);
+  return (
+    <div className=" flex text-center items-center justify-center min-h-dvh">
+      <div>
+        <h2 className="text-xl font-bold">Opps...</h2>
+        <p>something wrong</p>
+        <p>{error.message}</p>
+        <button
+          onClick={() => reset()}
+          className="bg-black text-white py-2 px-5 rounded-lg shadow-md hover:bg-green-700 transition-all"
+        >
+          try again
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
